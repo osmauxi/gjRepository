572254699b29808c49f1eb5cc5450682a8ed54a8
贾宇轩 <11350150+jia-yuxuan12@user.noreply.gitee.com>
2026-01-30 17:04:49 +0800

    NGO

---
+++ Assets/Scripts/InterfaceBlock.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InterfaceBlock : MonoBehaviour
+{
+    public interface IEnegyBall 
+    {
+        public void Get();
+    }
+
+    public interface IMask
+    {
+        public void ExcuteSkill();
+    }
+
+}
